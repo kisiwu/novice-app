fe0129405d268d72a2e289a35b66dccaee26553f
Stephane Demingongo Litemo <sdemingongo@gmail.com>
2020-03-08 13:12:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# novice-app
+
+in development ...
---
+++ test/tests/00_setup.js
@@ -0,0 +1,5 @@
+require('chai/register-assert');  // Using Assert style
+require('chai/register-expect');  // Using Expect style
+require('chai/register-should');  // Using Should style
+
+require('debug').enable('@novice1/app*'); // Enable logs
