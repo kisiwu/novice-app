6d15916ea38658bb49121e488a65bdc8a9fe6800
demingongo <sdemingongo@gmail.com>
2021-09-02 00:06:16 +0200

    Update reply.ts

--- src/middlewares/reply.ts
+++ src/middlewares/reply.ts
@@ -3,9 +3,6 @@ import { IApp } from '../baseApp';
 
 export function addReplyMiddleware(app: IApp): core.RequestHandler  {
   return function replyMiddleware(_, res, next){
-  
-    // set functions
-    // res.reply = reply;
 
     // set headers
     if (app.enabled('x-powered-by'))  {
